4b78abc6d028a932a81f6c1569e4d18254d21ae1
yangliqiang <99463816@qq.com>
2023-08-22 22:54:26 +0800

    fixed go mod

--- Go-blog-server/core/gorm.go
+++ Go-blog-server/core/gorm.go
@@ -2,13 +2,15 @@ package core
 
 import (
 	"Go-blog-server/global"
+	"fmt"
 	"time"
 
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-func MySqlConnect() *gorm.DB {
+func InitGorm() *gorm.DB {
 	if global.Config.MySql.Host == "" {
 		global.LOG.Warn("未配置MySql,取消gorm链接.")
 		return nil
@@ -16,14 +18,15 @@ func MySqlConnect() *gorm.DB {
 
 	dsn := global.Config.MySql.Dsn()
 	var mysqlLogger logger.Interface
-	if global.CONFIG.System.Env == "dev"{
+
+	if global.Config.System.Env == "dev" {
 		mysqlLogger = logger.Default.LogMode(logger.Info)
 	} else {
 		mysqlLogger = logger.Default.LogMode(logger.Error)
 	}
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger.mysqlLogger
+		Logger: mysqlLogger,
 	})
 	if err != nil {
 		global.LOG.Error(fmt.Sprint("[%s] MySql连接失败", dsn))
@@ -31,7 +34,7 @@ func MySqlConnect() *gorm.DB {
 	}
 	sqlDB, _ := db.DB()
 	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpemConns(100)
+	sqlDB.SetMaxOpenConns(100)
 	sqlDB.SetMaxIdleConns(10)
 	sqlDB.SetConnMaxLifetime(time.Hour * 4)
 	return db

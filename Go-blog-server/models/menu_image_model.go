bc8dc839da391ea1f936b11a372b373a3ffb238b
yangliqiang <yangliqiang.xmov.ai>
2023-08-29 00:35:11 +0800

    update model

---
+++ Go-blog-server/models/menu_image_model.go
@@ -0,0 +1,9 @@
+package models
+
+type MenuImageModel struct {
+	MenuID      uint        `json:"menu_id"`
+	MenuModel   MenuModel   `gorm:"foreignKey:MenuID"`
+	BannerID    uint        `json:"image_id"`
+	BannerModel BannerModel `gorm:"foreignKey:BannerID"`
+	Sort        int         `gorm:"size:10" json:"sort"`
+}
--- Go-blog-server/models/menu_model.go
+++ Go-blog-server/models/menu_model.go
@@ -4,12 +4,12 @@ import "Go-blog-server/models/ctype"
 
 type MenuModel struct {
 	MODEL
-	MenuTitle    string       `gorm:"size:32" json:"menu_title"`
-	MenuTitleEn  string       `gorm:"size:32" json:"menu_title_en"`
-	Slogan       string       `gorm:"size:64" json:"slogan"`                                                                       //slogan
-	Abstract     ctype.Array  `gorm:"type:string" json:"abstract"`                                                                 //简介
-	AbstractTime *int         `json:"abstract_time"`                                                                               //简介的切换时间
-	MenuIMages   []ImageModel `gorm:"many2many:menu_image_models;jsonForeignKey:MenuID;JoinReferences:ImageID" json:"menu_images"` //菜单的图片列表
-	MenuTime     *int         `json:"menu_time"`                                                                                   //菜单图片的切换时间 为null表示不切换
-	Sort         int          `gorm:"size:10" json:"sort"`                                                                         //菜单的顺序
+	MenuTitle    string        `gorm:"size:32" json:"menu_title"`
+	MenuTitleEn  string        `gorm:"size:32" json:"menu_title_en"`
+	Slogan       string        `gorm:"size:64" json:"slogan"`                                                                           //slogan
+	Abstract     ctype.Array   `gorm:"type:string" json:"abstract"`                                                                     //简介
+	AbstractTime int           `json:"abstract_time"`                                                                                   //简介的切换时间
+	Banners      []BannerModel `gorm:"many2many:menu_banner_models;jsonForeignKey:MenuID;JoinReferences:BannerID" json:"banner_images"` //菜单的图片列表
+	BannerTime   int           `json:"banner_time"`                                                                                     //菜单图片的切换时间 为null表示不切换
+	Sort         int           `gorm:"size:10" json:"sort"`                                                                             //菜单的顺序
 }
